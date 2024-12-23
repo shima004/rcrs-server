e45e2c1d1c8a1df37e466fde02133ad2229d1dac
shima004 <yellow030408@gmail.com>
2024-12-23 16:12:37 +0900

    map_selecter.shの変数名を修正し、ウィンドウを一度だけ終了させるように変更

--- scripts/map_selecter.sh
+++ scripts/map_selecter.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 function kill_window() {
-  ./kill.sh
-  echo window was killed
-  #now time
-  cp -r ../logs/log ../archive/$mapname-$(date +%Y-%m-%d-%H-%M-%S)
-  echo "rescue log was saved to ../archive/$mapname-$(date +%Y-%m-%d-%H-%M-%S)"
+  if [ $count -eq 0 ]; then
+    count=$((count+1))
+    ./kill.sh
+    echo window was killed
+    #now time
+    cp -r ../logs/log ../archive/$map_name-$(date +%Y-%m-%d-%H-%M-%S)
+    echo "rescue log was saved to ../archive/$map_name-$(date +%Y-%m-%d-%H-%M-%S)"
+  fi
 }
 
 # trap ctrl-c
 trap kill_window SIGINT
 
 DIR=$(dirname $0)
+count=0
 
 # print the map folder
 for i in $(ls $DIR/../maps) 
@@ -22,14 +26,14 @@ done
 # check if map name is provided as an argument
 if [ -z "$1" ]; then
   echo "Please input the map name:"
-  read mapname
+  read map_name
 else
-  mapname=$1
+  map_name=$1
 fi
 
 # option
 NO_DUI=$2
 
-./start-comprun.sh -m ../maps/$mapname/map -c ../maps/$mapname/config $NO_DUI
+./start-comprun.sh -m ../maps/$map_name/map -c ../maps/$map_name/config $NO_DUI
 
 kill_window
